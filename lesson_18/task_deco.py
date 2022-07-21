3b0dacde8a2ea18063beb5b3650490fbd9564c46
Alba3k <alexbyby@gmail.com>
2022-07-21 20:12:19 +0300

    Update task_deco.py

--- lesson_18/task_deco.py
+++ lesson_18/task_deco.py
@@ -6,9 +6,13 @@ file_dict = {}
 def my_decorator(func):
     def wrapper():
         name_file = file_name
+        print(name_file)
         size_file = os.path.getsize(file_name)
+        print(size_file)
         url = os.path.abspath(file_name)
+        print(url)
         extension = url.split('.')[-1]
+        print(extension)
         file_dict[name_file] = (size_file, url, extension)
         lens = len(file_dict)
         print(f'Словарь содержит информацию о {lens} файлах')
