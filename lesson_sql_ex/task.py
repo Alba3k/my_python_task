1bef31f813c5123c02e1706c70214ff5b410b76a
Alba3k <alexbyby@gmail.com>
2022-08-15 17:06:06 +0300

    +++

---
+++ lesson_sql_ex/task.py
@@ -0,0 +1,15 @@
+# Задание 1
+SELECT model,speed,hd FROM pc
+WHERE price < 500
+
+# Задание 2
+SELECT DISTINCT maker FROM product
+WHERE type = 'Printer'
+
+# Задание 3
+SELECT model,ram,screen FROM laptop
+WHERE price > 1000
+
+
+
+
